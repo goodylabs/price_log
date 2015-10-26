39a0561f7dbfcc8345503b11e0e34d09c3a9d777
Andrzej Liśkiewicz <andrzej.liskiewicz@goodylabs.com>
2015-10-26 12:47:31 +0100

    return current value if possible

--- lib/price_log.rb
+++ lib/price_log.rb
@@ -17,6 +17,8 @@ module PriceLog
 
   module ClassMethods
 
+    UNDEFINED_CURRENT_VALUE_FIELD_NAME = "UNDEFINED_CURRENT_VALUE_FIELD_NAME"
+
     def has_price_log(name, options = {})
       configuration = {
         name: name,
@@ -28,12 +30,14 @@ module PriceLog
       _relation_name_ = "all_#{_field_name_}_price_logs".to_sym
       has_many _relation_name_, -> (object){ where("priceable_field_name = ?", _field_name_.to_sym)},  {as: :priceable, dependent: :destroy, class_name: "PriceLogEntry", validate:false}
 
-      _current_value_field_name_ = options[:current_value_field_name]
+      _current_value_field_name_ = options[:current_value_field_name] || UNDEFINED_CURRENT_VALUE_FIELD_NAME
 
       class_eval %{
         def #{_field_name_}(date=nil)
+          #{ (_current_value_field_name_ == UNDEFINED_CURRENT_VALUE_FIELD_NAME) ? "return self.#{_current_value_field_name_} if self.respond_to?('#{_current_value_field_name_}') && date.nil? " : '' }
           pl = self.#{_field_name_}_log(date)
           pl.price unless pl.nil?
+
         end
 
         def #{_field_name_}_log(date=nil)
@@ -43,7 +47,8 @@ module PriceLog
         def #{_field_name_}=(amount)
           ple = PriceLogEntry.new(priceable: self, price: amount, start_date: DateTime.now, priceable_field_name: '#{_field_name_}')
 
-          #{ !_current_value_field_name_.blank? ? "self.#{_current_value_field_name_} = amount if self.respond_to?('#{_current_value_field_name_}')" : ''}
+          #{ (_current_value_field_name_ == UNDEFINED_CURRENT_VALUE_FIELD_NAME) ? "self.#{_current_value_field_name_} = amount if self.respond_to?('#{_current_value_field_name_}')" : ''}
+
           self.#{_relation_name_} << ple
         end
 
--- lib/price_log/version.rb
+++ lib/price_log/version.rb
@@ -1,3 +1,3 @@
 module PriceLog
-  VERSION = "0.0.8"
+  VERSION = "0.0.81"
 end
